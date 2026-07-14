998eec8ae4df3aff6ef4fc40aff0cba5ebb94b77
expertyeti <leivetd@gmail.com>
2026-07-14 14:32:45 -0600

    borramos automaticamente los archivos temporal es y ccarpetas de versoines sin terminar de instalarse al iniciar de vuelta el launcher

--- app/src/core/scripts.js
+++ app/src/core/scripts.js
@@ -4,20 +4,25 @@ import { registerHomeView } from "../ui/home/index.js";
 import { registerEnginesView } from "../ui/engines/index.js";
 import { downloadEngine } from "../ui/engines/downloadEngine.js";
 import { disableProductionRefreshShortcuts } from "./productionShortcuts.js";
+import { FS } from "../utils/filesystem.js"; 
 
 async function startApp() {
   try {
     Neutralino.init();
     disableProductionRefreshShortcuts();
+    
     Neutralino.events.on("windowClose", async () => {
       await downloadEngine.cleanupAll();
       await Neutralino.app.exit();
     });
 
+    await FS.init();
+
     await storageBridge.init();
     registerHomeView();
     registerEnginesView();
     await router.init();
+    
     console.log("WeekBox: modules loaded.");
   } catch (error) {
     console.error("Startup error:", error);
